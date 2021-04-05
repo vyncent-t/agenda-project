78896ff3a2d5bac83416ee624e9efc96240b3894
Vincent T <vyncode@gmail.com>
2021-04-04 22:16:47 -0400

    set up time logic for each hour

--- PLANME.md
+++ PLANME.md
@@ -1,17 +1,17 @@
 NEED TO:
 
-A. DISPLAY CURRENT DAY AS MAIN TITLE
-    - Title content already exist, need to change the title content with updating moment.js tools
+<!-- A. DISPLAY CURRENT DAY AS MAIN TITLE
+    - Title content already exist, need to change the title content with updating moment.js tools -->
 
-B. CREATE TIMEBLOCKS FOR 9-5 WORK DAY
-    - Create grid layout with bootstrap for every hour that exists in the work day
-    -
+<!-- B. CREATE TIMEBLOCKS FOR 9-5 WORK DAY
+    - Create grid layout with bootstrap for every hour that exists in the work day -->
 
 C. COLOR TIMEBLOCKS FOR PAST PRESENT AND FUTURE
     - Exist in base CSS?
 
 D. WHEN CLICKING ON A TIME BLOCK, CAN ENTER AN EVENT
     - Need to make it so you cannot add to a time that has passsed?
+    - Use inputs for user input
 
 E. SAVE BUTTON THAT STORES THE USER INPUT EVENT TO THE LOCAL STORAGE
     - Events must be static on refresh
@@ -23,3 +23,9 @@ Keep inputs static on page? not the "" function?
 jQuery functions for when the time right now timeRN class is set to current 
 - if time is less than time rn make gray
 - if the time is not yet then blue
+
+
+
+loop over all times and set a data for the time in 24 time hours mode
+
+if data time compared to time now do some
--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 // current date and time moment js
-let mtime = moment();
+let now = moment();
+
+
 
 // month day year, time
-console.log(mtime.toString())
+console.log(now.toString())
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
 // timer functionality
 setInterval(() => {
     let userTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $('#currentDay').text(userTime)
-}, 1000);
+    $('#currentDay').text(userTime)}, 1000);
+
+
+//current hour 
+let currentHour = moment().format('H')
+console.log(currentHour)
+
+
+//Calculate the number of hours from now and a given hour (in 24 hours format) of the day
+// If result is <0 means the given time is in the future
+// If result is >0 means the given time is in the past
+function hoursFromNow(time) {
+  //Create an instance of moment for the current time
+  var now=moment();
+  //Create an instance of moment for the given time
+  var todayTime = moment(now.format("MM/DD/YYYY")+" "+ time +":00");
+  //Return the difference in hours between the two moments
+  return now.diff(todayTime,"hours");
+}
+
+let workblocks = document.querySelectorAll('.timepiece')
+
+    for (let card of workblocks) { 
+        if (card.dataset.basetime < currentHour){
+            console.log(`${card.dataset.basetime} is in the past, set to gray`)
+            if (card.dataset.basetime === currentHour) {
+                console.log(`The work hour rn is ${card.dataset.basetime}`)
+            }
+        } if (card.dataset.basetime > currentHour) {console.log(`${card.dataset.basetime} in the future set to blue`)}
+    }
+
+
